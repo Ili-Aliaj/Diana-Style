d2fccb0122d0f10b713af624e223306708558d24
Ili Aliaj <ilialiaj3@gmail.com>
2024-03-26 16:10:36 +0100

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import HomePageTitle from './Head Components/HomePageTitle.jsx';
 import Icon from './Head Components/Icon.jsx';
 import Header from './BodyComponents/Header.jsx';
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   
 
